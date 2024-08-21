94b9d8ff2cc784ae8cf55d6c18acac202789a2c2
karim kammoun <karimsfax06@gmail.com>
2024-08-21 23:14:32 +0100

    Leet_Code

--- Easy/26..Remove_Duplicates_from_Sorted_Array.py
+++ Easy/26..Remove_Duplicates_from_Sorted_Array.py
@@ -1,17 +1,12 @@
-nums = [0,0,1,1,1,2,2,3,3,4]
-
-
-k=0
-i=1
-while i<len(nums) :
-    if nums[i]>nums[k] and nums[k]==nums[k+1]:
-        nums[k+1]=nums[i]
-        k=k+1
-    i=i+1
-    print(nums)
-
-
-
-
-print(nums)
-print(k) 
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        k=0
+        for i in range(1,len(nums)):
+            if nums[i]>nums[k]:
+                nums[k+1]=nums[i]
+                k=k+1
+        return(k+1) 
